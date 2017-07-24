5cb65b0d147a7a16698250cc4726a90d4330804d
skynol <lyons.keith@gmail.com>
2017-07-24 13:43:42 +0100

    updated mongodb acces code

--- Python/mongodb.py
+++ Python/mongodb.py
@@ -1,19 +1,20 @@
 from pymongo import Connection
 
 if __name__ == "__main__":
-    con = Connection()
+    con = Connection('0.0.0.0', 27017)
 
     db = con.test_database
 
     people = db.people
 
-    people.insert({'name':'Mike', 'food':'cheese'})
-    people.insert({'name':'John', 'food':'ham', 'Location':'UK'})
-    people.insert({'name':'Michelle', 'food':'cheese'})
-    
+    #people.insert({'name':'Mike', 'food':'cheese'})
+    #people.insert({'name':'John', 'food':'ham', 'Location':'UK'})
+    #people.insert({'name':'Michelle', 'food':'cheese'})
+
     peeps = people.find()
 
     print "INSERT & FIND TEST"
 
     for person in peeps:
+        if(person.name = "Mike")
         print person
